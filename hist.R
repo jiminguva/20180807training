d7e2475e9d598a236851f6fe4d960bcdd1c326a3
jiminguva <40008158+jiminguva@users.noreply.github.com>
2018-08-07 10:02:08 -0400

    remove the unfunctional script

--- hist.R
+++ hist.R
@@ -12,6 +12,4 @@ abline(v = xbar, lwd = 2, col = "red")
 abline(v = xbar + 2*s, lwd = 2, col = "red", lty = 3)
 abline(v = xbar - 2*s, lwd = 2, col = "red", lty = 3)
 
-# abline(v = xbar, lwd = 2, col = "red")
-# abline(v = xbar + 2*s, lwd = 2, col = "red", lty = 3)
-# abline(v = xbar - 2*s, lwd = 2, col = "red", lty = 3)
+
