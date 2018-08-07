19c5f200b06fbc2873151d45820b9ee176a96b01
jiminguva <40008158+jiminguva@users.noreply.github.com>
2018-08-07 09:57:45 -0400

    edit the files with several line of not func scrip

--- hist.R
+++ hist.R
@@ -11,3 +11,7 @@ hist(smpl)
 abline(v = xbar, lwd = 2, col = "red")
 abline(v = xbar + 2*s, lwd = 2, col = "red", lty = 3)
 abline(v = xbar - 2*s, lwd = 2, col = "red", lty = 3)
+
+# abline(v = xbar, lwd = 2, col = "red")
+# abline(v = xbar + 2*s, lwd = 2, col = "red", lty = 3)
+# abline(v = xbar - 2*s, lwd = 2, col = "red", lty = 3)
